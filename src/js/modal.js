34b6ccf6743c10257480058b2946d01956e1811d
bod111 <86015208+bod111@users.noreply.github.com>
2024-05-04 18:32:38 +0300

    fix(modal): update styles animation, isert  and stilisation svg

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,9 +1,9 @@
 const modalContainerRef = document.querySelector('.modal-container');
-const modalBackdropRef = document.querySelector('.modal-bg');
 const modalRef = document.querySelector('.modal');
 const modalCloseRef = document.querySelector('.modal-close');
 const modalTitleRef = document.querySelector('.modal-title');
 const modalTextRef = document.querySelector('.modal-text');
+const socialIconsRef = document.querySelector('.social-icons');
 
 const modalOpenRef = document.querySelector('.btn-open-modal');
 modalOpenRef.addEventListener('click', mdShow);
@@ -11,30 +11,22 @@ modalOpenRef.addEventListener('click', mdShow);
 function mdShow() {
   modalContainerRef.classList.add('visible');
   document.body.classList.add('body-overflow');
-  console.log('mdShow');
   modalCloseRef.addEventListener('click', mdClose);
-  modalBackdropRef.addEventListener('click', mdClose);
+  modalContainerRef.addEventListener('click', eventsOnModal);
   window.addEventListener('keydown', eventsOnModal);
 }
 
 function eventsOnModal(e) {
+  if (e.target === e.currentTarget) mdClose();
   if (e.key === 'Escape') mdClose();
 }
 
 function mdClose() {
-  // console.log('mdClose');
   modalContainerRef.classList.remove('visible');
   document.body.classList.remove('body-overflow');
   modalCloseRef.removeEventListener('click', mdClose);
-  modalBackdropRef.removeEventListener('click', mdClose);
+  modalContainerRef.removeEventListener('click', mdClose);
   window.removeEventListener('keydown', eventsOnModal);
-}
-
-function renderSocial() {
-  return `<ul class="social-icons">
-  <li><a class="icon" href="" target="_blank"></a></li>
-  <li><a class="icon" href="" target="_blank"></a></li>
-  </ul>`;
 }
 
 function onErrorModalContant() {
@@ -42,7 +34,7 @@ function onErrorModalContant() {
   modalTitleRef.textContent = 'На жаль, наразі наш сервер не відповідає!';
   modalTextRef.textContent =
     'Будь ласка, скористайтеся соціальними мережами експерта  для отримання зворотнього зв’язку:';
-  modalTextRef.insertAdjacentHTML('afterend', renderSocial());
+  socialIconsRef.classList.remove('hidden-icons');
 }
 
 onErrorModalContant();
