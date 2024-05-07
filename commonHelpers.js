81daaac374e8527ddbaaab1de29d5191605e2305
SofiaProtsiv <71260387+SofiaProtsiv@users.noreply.github.com>
2024-05-07 20:51:14 +0000

    Deploying to gh-pages from @ SofiaProtsiv/numismatics-project@7b7cec2deb18cc1316798506962a43df10ebed7d 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import"./assets/main-4d5b230a.js";import"./assets/vendor-4ea312d0.js";const e=document.querySelector(".faq-nav-home"),o=document.querySelector(".faq-nav-reload"),n=()=>{window.location.href="/"},t=()=>{window.location.href="../faq-full.html"};e.addEventListener("click",n);o.addEventListener("click",t);
+import"./assets/main-4d5b230a.js";import"./assets/vendor-4ea312d0.js";const e=document.querySelector(".faq-nav-home"),o=document.querySelector(".faq-nav-reload"),n=()=>{window.location.href="/"},t=()=>{window.location.href="/faq-full.html"};e.addEventListener("click",n);o.addEventListener("click",t);
 //# sourceMappingURL=commonHelpers.js.map
