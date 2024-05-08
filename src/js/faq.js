d61ee57fcf67c9473b80f57bd0f89339547093a4
SergeiAntonenko <gerbar50@gmail.com>
2024-05-08 16:01:16 +0300

    fix(faq-section): fix js logic

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,25 +1,3 @@
-import localizeElements from './localization';
-
-const savedLang = localStorage.getItem('currentLang') || 'ua';
-
-if (savedLang) {
-  document.getElementById('langSelect').value = savedLang;
-  console.log(savedLang);
-
-  localizeElements(savedLang);
-}
-
-document
-  .querySelector('#langSelect')
-  .addEventListener('change', changeLanguage);
-
-function changeLanguage() {
-  const currentLang = document.getElementById('langSelect').value;
-  localizeElements(currentLang);
-
-  localStorage.setItem('currentLang', currentLang);
-}
-
 document.querySelectorAll('.faq-list-item').forEach(item => {
   const itemContainer = item.querySelector('.faq-list-item-container');
 
