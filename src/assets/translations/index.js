8a13f5eb76c8c5d5646c02c25ea2b9f82bfaa9f0
SergeiAntonenko <gerbar50@gmail.com>
2024-05-08 15:45:03 +0300

    feat(faq-section): add localization

--- src/assets/translations/index.js
+++ src/assets/translations/index.js
@@ -3,33 +3,41 @@ const localizationData = {
     about: {
       expertise: 'ЕКСПЕРТНІСТЬ',
       title: 'ПРО МЕНЕ',
-      firstParagraph: 'Завжди вважав, що моє хобі може стати професією. Усе почалося в дитинстві, коли у десять років я ' +
+      firstParagraph:
+        'Завжди вважав, що моє хобі може стати професією. Усе почалося в дитинстві, коли у десять років я ' +
         'зібрав свою першу колекцію та мріяв про кар’єру археолога. Ця мрія стала реальністю у 2009 році, коли я вступив ' +
         'на історичний факультет Київського національного університету імені Тараса Шевченка та навчався на ' +
         'кафедрі археології та музеєзнавства.',
-      secondParagraph: 'Під час навчання я також здобув практичні навички у міжнародній бізнес-спільності, що дозволило ' +
+      secondParagraph:
+        'Під час навчання я також здобув практичні навички у міжнародній бізнес-спільності, що дозволило ' +
         'мені розпочати свою підприємницьку діяльність.У 2013 році я став співвласником компанії «Монітекс», що ' +
         'спеціалізується на нумізматиці, боністиці, фалеристиці та інших суміжних напрямках.',
-      thirdParagraph: 'Ми допомагаємо нашим клієнтам створювати та поповнювати їхні колекції, оцінюємо старовинні ' +
+      thirdParagraph:
+        'Ми допомагаємо нашим клієнтам створювати та поповнювати їхні колекції, оцінюємо старовинні ' +
         'предмети та визначаємо їхню ідентичність, пропонуємо найбільший асортимент колекційних товарів в Україні. ' +
         'Ми прагнемо розвивати культуру колекціонування в нашій країні, вносячи свій вклад у збереження та поширення ' +
         'цієї традиції.',
-      fourthParagraph: 'Крім того, я є резидентом клубу підприємців PRIZMA CLUB, де разом із колегами обговорюємо ' +
+      fourthParagraph:
+        'Крім того, я є резидентом клубу підприємців PRIZMA CLUB, де разом із колегами обговорюємо ' +
         'новітні тенденції у бізнесі та культурі, ділимося досвідом та шукаємо нові можливості для розвитку в Україні.',
     },
 
     areas: {
       category: 'КАТЕГОРІЇ',
       title: 'НАПРЯМКИ ЕКСПЕРТИЗИ',
-      content: 'Компанія «Монітекс» була заснована в 2013 році і з того часу здобула довіру багатьох постійних клієнтів. ' +
+      content:
+        'Компанія «Монітекс» була заснована в 2013 році і з того часу здобула довіру багатьох постійних клієнтів. ' +
         'У нас працюють експерти з унікальними знаннями та багаторічним досвідом.',
       numismaticTitle: 'Нумізматика',
-      numismaticContent: 'Консультую клієнтів щодо купівлі та продажу монет, допомагаю визначити їх оригінальність.',
+      numismaticContent:
+        'Консультую клієнтів щодо купівлі та продажу монет, допомагаю визначити їх оригінальність.',
       bonisticTitle: 'Боністика',
-      bonisticContent: 'Оцінюю старовинні та сучасні банкноти, розповідаю про можливості інвестування в паперові гроші. ' +
+      bonisticContent:
+        'Оцінюю старовинні та сучасні банкноти, розповідаю про можливості інвестування в паперові гроші. ' +
         'Ділюся знаннями як розрізняти унікальні екземпляри.',
       faleristicTitle: 'Фалеристика',
-      faleristicContent: 'Визначаю оригінальність медалей та нагород, які мають не тільки матеріальну, але й історичну ' +
+      faleristicContent:
+        'Визначаю оригінальність медалей та нагород, які мають не тільки матеріальну, але й історичну ' +
         'цінність. Консультую з питань купівлі та продажу таких предметів.',
     },
 
@@ -45,7 +53,8 @@ const localizationData = {
       subTitle: 'Консультації',
       title: 'Послуги',
       text: 'Пропоную професійне консультування з інвестування в золоті та срібні монети: допоможу вам зрозуміти ринкові тенденції та вибрати найкращі варіанти для формування та збільшення вартості вашого портфелю. Також надаю послуги зі створення та розширення колекцій через підтримку - від вибору до купівлі.',
-      cardTitleFirst: 'Консультування з питань інвестування у золоті та срібні монети',
+      cardTitleFirst:
+        'Консультування з питань інвестування у золоті та срібні монети',
       hashtagInvest: '#інвестування',
       hashtagGold: '#золото',
       hashtagSilver: '#срібло',
@@ -91,7 +100,8 @@ const localizationData = {
 
     footer: {
       location: 'Розташування:',
-      address: 'м. Київ, вул. Богдана Хмельницького 3, поверх 2 (вхід з вулиці)',
+      address:
+        'м. Київ, вул. Богдана Хмельницького 3, поверх 2 (вхід з вулиці)',
       website: 'Сайт:',
       socialCompany: 'Соціальні мережі компанії:',
       socialExpert: 'Соціальні мережі експерта:',
@@ -100,23 +110,68 @@ const localizationData = {
     teamModal: {
       title: 'Команда, яка створила сайт',
     },
+
+    faq: {
+      title: 'відповіді',
+      subTitle:
+        'Компанія «Монітекс» була заснована в 2013 році і з того часу здобула довіру багатьох постійних клієнтів. У нас працюють експерти з унікальними знаннями та багаторічним досвідом.',
+      'full-nav-1': 'головна',
+      'full-nav-2': 'Про мене',
+      'full-nav-3': 'Експертиза',
+      'full-nav-4': 'Послуги',
+      'full-nav-5': 'Відгуки клієнтів',
+      'autor-name': 'Іван Прудько',
+      'autor-info':
+        'Співзасновник та експерт компанії «Монітекс» - інтернет-магазину №1 для колекціонерів в Україні.',
+      'question-1': 'Що таке нумізматика?',
+      'question-2': 'Як почати колекціонувати монети?',
+      'question-3': 'Де купувати колекційні предмети?',
+      'question-4': 'Чому консультуватися та купувати саме у нас?',
+      'question-5': 'Як визначити вартість старовинної монети або банкноти?',
+      'question-6':
+        'Які послуги ви надаєте для колекціонерів банкнот, нагород та орденів?',
+      'question-7':
+        'Чому інвестування в золоті та срібні монети може бути вигідним?',
+      'question-8': 'Як забезпечити належне зберігання колекційних предметів?',
+
+      'answer-1':
+        'Нумізматика – це спеціальна історична дисципліна, що вивчає монети як джерело історичних, економічних, політичних і культурних знань, а також історію грошового обігу та монетного виробництва, процеси фальшування монет та банкнот. Також слово нумізматика використовується на позначення різновиду колекціонування.',
+      'answer-2':
+        'Для початку збирання монетних колекцій рекомендую визначити інтереси у певних типах монет або історичних періодах, встановити бюджет та консультуватися з експертами для кращого розуміння ринку.',
+      'answer-3':
+        'На сайті monitex.com.ua запропонована виключно оригінальна продукція, яка перевірена експертами з більш, ніж 10-річним практичним досвідом та унікальними знаннями.',
+      'answer-4':
+        'Тому що я маю унікальні знання, які отримав разом з вищою освітою на історичному факультеті та щоденним практичним досвідом з 2013 року.',
+      'answer-5':
+        'Вартість старовинної монети чи банкноти залежить від її рідкості, стану збереження, попиту серед колекціонерів та історичної значимості. Я можу оцінити ваші предмети та дати професійну рекомендацію.',
+      'answer-6':
+        'Послуги з визначення автентичності та оригінальності предмета. Також оцінюю та консультую щодо купівлі та продажу банкнот, нагород та орденів.',
+      'answer-7':
+        'Інвестування в золоті та срібні монети вважається стабільним та безпечним способом збереження капіталу, особливо в часи економічної невизначеності, оскільки цінні метали традиційно зберігають свою вартість.',
+      'answer-8':
+        'Для зберігання колекційних предметів важливо використовувати спеціальні матеріали, які запобігають окисленню, а також забезпечувати оптимальні умови температури та вологості.',
+    },
   },
   en: {
     about: {
       expertise: 'EXPERTISE',
       title: 'ABOUT ME',
-      firstParagraph: 'I always believed that my hobby could become my profession. It all started in childhood, when ' +
+      firstParagraph:
+        'I always believed that my hobby could become my profession. It all started in childhood, when ' +
         'at the age of ten I collected my first collection and dreamed of a career as an archaeologist. This dream ' +
         'became a reality in 2009, when I entered the Faculty of History of Taras Shevchenko Kyiv National University ' +
         'at the Department of Archeology and Museum Studies',
-      secondParagraph: 'During my studies, I also gained practical skills in the international business community, ' +
+      secondParagraph:
+        'During my studies, I also gained practical skills in the international business community, ' +
         'which allowed me to start my own business. In 2013, I became a co-owner of the Monitex company, ' +
         'which specializes in numismatics, bonistics, phaleristics and other related areas.',
-      thirdParagraph: 'We help our customers create and replenish collections, evaluate antique items and determine ' +
+      thirdParagraph:
+        'We help our customers create and replenish collections, evaluate antique items and determine ' +
         'their identity, and we also offer the largest assortment of collectible goods in Ukraine. We strive to ' +
         'develop the culture of collecting in our country, contributing to the preservation and dissemination of ' +
         'this tradition.',
-      fourthParagraph: 'In addition, I am a resident of the PRIZMA CLUB entrepreneurs\' club, where together with ' +
+      fourthParagraph:
+        "In addition, I am a resident of the PRIZMA CLUB entrepreneurs' club, where together with " +
         'colleagues we discuss the latest trends in business and culture, share experience and look for new ' +
         'opportunities for development in Ukraine.',
     },
@@ -124,15 +179,19 @@ const localizationData = {
     areas: {
       category: 'CATEGORIES',
       title: 'AREAS OF EXPERTISE',
-      content: 'The company "Monitex" was founded in 2013 and since then has gained the trust of many regular customers. ' +
+      content:
+        'The company "Monitex" was founded in 2013 and since then has gained the trust of many regular customers. ' +
         'We employ experts with unique knowledge and many years of experience.',
       numismaticTitle: 'Numismatics',
-      numismaticContent: 'I advise clients on the purchase and sale of coins, help determine their originality.',
+      numismaticContent:
+        'I advise clients on the purchase and sale of coins, help determine their originality.',
       bonisticTitle: 'Bonistics',
-      bonisticContent: 'I evaluate old and modern banknotes, talk about the possibilities of investing in paper money. ' +
+      bonisticContent:
+        'I evaluate old and modern banknotes, talk about the possibilities of investing in paper money. ' +
         'I share knowledge on how to distinguish unique specimens.',
       faleristicTitle: 'Faleristics',
-      faleristicContent: 'I determine the originality of medals and awards, which have not only material, ' +
+      faleristicContent:
+        'I determine the originality of medals and awards, which have not only material, ' +
         'but also historical value. I advise on buying and selling such things.',
     },
 
@@ -190,12 +249,14 @@ const localizationData = {
     },
 
     contact: {
-      title: "Do you have any questions or would you like to avail of our services?"
+      title:
+        'Do you have any questions or would you like to avail of our services?',
     },
 
     footer: {
       location: 'Location:',
-      address: '3 Bohdana Khmelnytskoho Street, Kyiv, 2nd floor (entrance from the street)',
+      address:
+        '3 Bohdana Khmelnytskoho Street, Kyiv, 2nd floor (entrance from the street)',
       website: 'Website:',
       socialCompany: 'Companys Social Media:',
       socialExpert: 'Experts Social Media:',
@@ -203,6 +264,46 @@ const localizationData = {
     },
     teamModal: {
       title: 'The team that created the site',
+    },
+
+    faq: {
+      title: 'answers',
+      subTitle:
+        'The company "Monitex" was founded in 2013 and since then has gained the trust of many regular customers. We employ experts with unique knowledge and many years of experience.',
+      'full-nav-1': 'main',
+      'full-nav-2': 'About me',
+      'full-nav-3': 'Areas of expertise',
+      'full-nav-4': 'Services',
+      'full-nav-5': 'Reviews',
+      'autor-name': 'Ivan Prudko',
+      'autor-info':
+        'Co-founder and expert of "Monitex" company - the number one online store for collectors in Ukraine.',
+      'question-1': 'What is numismatics?',
+      'question-2': 'How do I start collecting coins?',
+      'question-3': 'Where can I buy collectible items?',
+      'question-4': 'Why should I consult with and buy from you?',
+      'question-5':
+        'How can I determine the value of an antique coin or banknote?',
+      'question-6':
+        'What services do you provide for collectors of banknotes, awards, and orders?',
+      'question-7': 'Why can investing in gold and silver coins be beneficial?',
+      'question-8': 'How can I ensure proper storage of collectible items?',
+      'answer-1':
+        'Numismatics is a specialized historical discipline that studies coins as a source of historical, economic, political, and cultural knowledge. It also encompasses the history of monetary circulation and coin production, as well as processes of coin counterfeiting and the study of banknotes. The term "numismatics" is also used to refer to a type of collecting.',
+      'answer-2':
+        'To start collecting coins, I recommend identifying specific types or historical periods of interest, setting a budget, and consulting with experts for a better understanding of the market.',
+      'answer-3':
+        'The website monitex.com.ua offers exclusively original products verified by experts with over 10 years of practical experience and unique knowledge.',
+      'answer-4':
+        'Because I possess unique knowledge acquired through higher education in the History Faculty and daily practical experience since 2013.',
+      'answer-5':
+        'The value of an antique coin or banknote depends on its rarity, condition, demand among collectors, and historical significance. I can evaluate your items and provide professional recommendations.',
+      'answer-6':
+        'Services include determining the authenticity and originality of the item. I also assess and advise on the buying and selling of banknotes, awards, and orders.',
+      'answer-7':
+        'Investing in gold and silver coins is considered a stable and secure way to preserve capital, especially in times of economic uncertainty, as precious metals traditionally retain their value.',
+      'answer-8':
+        'It is important to use special materials to store collectible items to prevent oxidation and ensure optimal temperature and humidity conditions.',
     },
   },
 };
