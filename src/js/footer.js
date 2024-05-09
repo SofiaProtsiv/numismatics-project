38ad32c64f615c67305c254a03ee0d72677f2ae0
Sofia Protsiv <prochivsofia@gmail.com>
2024-05-09 11:47:34 +0300

    fix(footer): add additional margin in en version

--- src/js/footer.js
+++ src/js/footer.js
@@ -6,3 +6,9 @@ const handleClick = function () {
 }
 
 logoSpan.addEventListener('click', handleClick);
+
+
+export function applyFooterMargin(lang) {
+    const el = document.querySelector('.container-website')
+    lang === 'en' ? el.classList.add('margin-english') : el.classList.remove('margin-english')
+}
